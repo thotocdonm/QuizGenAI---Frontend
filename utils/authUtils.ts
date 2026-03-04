45e9deb867e05d1a1fee171e050ed48ca28239eb
Loc0811 <locvuong417@gmail.com>
2026-03-04 17:40:45 +0700

    fix bug

--- utils/authUtils.ts
+++ utils/authUtils.ts
@@ -14,4 +14,5 @@ export const getRefreshToken = () => {
 export const clearToken = () => {
     localStorage.removeItem("accessToken");
     localStorage.removeItem("refreshToken");
+    localStorage.removeItem("user");
 }
